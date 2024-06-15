1fc439811901cd02d6ec2d49210152ac9eccca38
Igor Silva <igorqa2022@outlook.com>
2024-06-15 17:42:01 -0300

    Feat: Adicionado viewport para mobile

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,22 +2,37 @@
 
 import login_page from "../support/login_page/login_page";
 
-describe('Login', () => {
-  it('Validando mensagem quando nao informa senha', () => {
-    login_page.senhaVazia()
-    cy.get('#user').should('be.visible')
-    cy.get('#password').should('be.empty')
-    cy.get('.invalid_input').should('be.visible').and('have.text', 'Senha inválida.')
-  })
+const screens = ['desktop', 'iphone-xr', 'macbook-13']
 
-  it('Validando email incorreto', () => {
-    login_page.EmailInvalido()
-    cy.contains('.invalid_input', 'E-mail inválido.').should('be.visible')
-  })
+screens.forEach(screen => {
+  describe(`Login tests on ${screen}`, () => {
 
-  it('Login com sucesso', () => {
-    login_page.loginPage()
-    cy.get('#swal2-title').should('be.visible')
-    cy.get('#swal2-html-container').should('be.visible')
+    beforeEach(() => {
+      if (screen === 'desktop') {
+        cy.viewport('macbook-15')
+      } else if (screen === 'iphone-xr') {
+        cy.viewport('iphone-xr')
+      } else if (screen === 'macbook-13') {
+        cy.viewport('macbook-13')
+      }
+    });
+
+    it('Validando mensagem quando nao informa senha', () => {
+      login_page.senhaVazia()
+      cy.get('#user').should('be.visible')
+      cy.get('#password').should('be.empty')
+      cy.get('.invalid_input').should('be.visible').and('have.text', 'Senha inválida.')
+    });
+
+    it('Validando email incorreto', () => {
+      login_page.EmailInvalido()
+      cy.contains('.invalid_input', 'E-mail inválido.').should('be.visible')
+    });
+
+    it('Login com sucesso', () => {
+      login_page.loginPage()
+      cy.get('#swal2-title').should('be.visible')
+      cy.get('#swal2-html-container').should('be.visible')
+    });
   });
-})
+});
